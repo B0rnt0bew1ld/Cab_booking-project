5fe97d6a6ac16f6e47ca9d79d59d1cb15357afab
B0rnt0bew1ld <136583094+B0rnt0bew1ld@users.noreply.github.com>
2026-09-22 11:40:42 +0530

    second commit

--- cabx/cabs/models.py
+++ cabx/cabs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Station(models.Model):
+    pin_code = models.IntegerField()
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"City name : {self.city} and Pincode : {self.pin_code}"
+
+class Cabs(models.Model):
+    pick = models.CharField(max_length=64)
+    drop = models.CharField(max_length=64)
+    cost = models.IntegerField()
+
+    def __str__(self):
+        return f"Your picking point : {self.pick}, dropping point : {self.drop} and cost : Rs {self.cost}."
+    
--- cabx/cabs/views.py
+++ cabx/cabs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Cabs
 
 # Create your views here.
 def index(request):
-    return render(request, "cabs/index.html")
+    return render(request, "cabs/index.html",{
+        "cabs" : Cabs.objects.all()
+    })
+
